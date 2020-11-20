1068562633629e1707227ca796760af2e4acc84e
Naomi-Rhames <naomirhames2020@gmail.com>
2020-11-20 17:40:15 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,37 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+    true
+    else
+      false
+    end
     
+    #binding.pry
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+   save_word = self.split(/[.!?]/) 
+   save_word.delete("")
+    self.split.length
+    save_word.length
+#binding.pry
 
   end
 end
